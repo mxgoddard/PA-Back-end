1ab0f1304aac74e76589b03927cc3b5a2a67b6a6
Max Goddard <maxolivergoddard@gmail.com>
2018-11-26 12:14:48 +0000

    Fix unauthorized client access error

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const PORT = process.env.PORT || 9090;
 const app = require('./app');
 
-
-// app.get('/', (req, res) => {
-//   res.send('Connected');
-// });
-
 app.listen(PORT, (err) => {
   if (err) throw err;
   console.log(`Server listening on port ${PORT}`);
