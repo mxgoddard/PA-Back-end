daa56fb69f18358dff3f997c98a794315c6d4f68
Max Goddard <maxolivergoddard@gmail.com>
2018-11-27 19:12:43 +0000

    Add front-end entry point and start to integrate with databases

---
+++ controllers/entry.js
@@ -0,0 +1,39 @@
+const axios = require('axios');
+
+exports.entryPoint = (req, res) => {
+  /*
+    - This is our point of contact for the app, it should always connect to this one point when you
+    click on an event.
+
+    - Each trip has an ID which is used to reference it in the database. This needs to be passed in
+    query to the back-end.
+
+    - Check what data exists for the ID in the database. For the information that doesn't exist, run
+    functions to gather it, or as last resort, prompt user for it.
+
+    - Eventually store this data locally until user closes app.
+
+    - It's preferable to query our database than run api calls everytime.
+
+      tbl_trip
+    |---------
+    | user_id | trip_id | journey_id | summary | meeting_location | meeting_time
+    |---------
+
+      tbl_journey
+    |---------
+    | journey_id | depart_location | depart_time | arrive_location | arrive_time | duration | company
+    |---------
+
+      tbl_user
+    |---------
+    | user_id | first_name | last_name | email | login_token
+    |---------
+  */
+      
+  const { id } = req.query;
+
+  // Query database for information off the id.
+
+  res.send('Entry Point');
+};
--- router/routes.js
+++ router/routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { getEvents, landing, travel } = require('../controllers/controller');
 const { direction } = require('../controllers/directions');
+const { entryPoint } = require('../controllers/entry.js');
 
 router
   .route('/events')
@@ -17,5 +18,9 @@ router
 router
   .route('/direction')
   .get(direction);
+
+router
+  .route('/entry')
+  .get(entryPoint);
 
 module.exports = router;
