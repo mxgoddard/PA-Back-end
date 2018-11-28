f7760497eb9f4e377cb06e8a52795fa0a31f27b5
Stefano Artoni <stefano@Stefanos-MacBook-Pro.local>
2018-11-28 15:47:26 +0000

    can connect ot firebase (we are very proud)

--- controllers/entry.js
+++ controllers/entry.js
@@ -1,39 +1,24 @@
 const axios = require('axios');
+const admin = require('firebase-admin');
+const serviceAccount = require('../config/firebaseServiceAcc.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://projectpa-223310.firebaseio.com'
+});
+
 
 exports.entryPoint = (req, res) => {
-  /*
-    - This is our point of contact for the app, it should always connect to this one point when you
-    click on an event.
 
-    - Each trip has an ID which is used to reference it in the database. This needs to be passed in
-    query to the back-end.
+  const db = admin.firestore();
+  const newData = {
+    email: 'ginodacampo@gmail.com',
+    first_name: 'gino',
+    last_name: 'Dacampo',
+  };
+  const setDoc = db.collection('tbl_user').doc('hello').set(newData);
 
-    - Check what data exists for the ID in the database. For the information that doesn't exist, run
-    functions to gather it, or as last resort, prompt user for it.
 
-    - Eventually store this data locally until user closes app.
-
-    - It's preferable to query our database than run api calls everytime.
-
-      tbl_trip
-    |---------
-    | trip_id | user_id |  | summary | meeting_location | meeting_time | depart_location | depart_time | arrive_location | arrive_time | duration | company
-    |---------
-
-      tbl_user
-    |---------
-    | user_id | first_name | last_name | email | login_token
-    |---------
-
-      tbl_userTrip
-    |---------
-    | id | trip_id | user_id
-    |---------
-  */
-    
   const { id } = req.query;
-
-  // Query database for information off the id.
-
   res.send('Entry Point');
 };
