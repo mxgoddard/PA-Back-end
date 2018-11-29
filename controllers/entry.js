7dfe3763d4e2c47b68bccd8e8c1f0830b579bed7
Stefano Artoni <stefano@Stefanos-MacBook-Pro.local>
2018-11-29 16:14:37 +0000

    retrieve dynamic location data

--- controllers/entry.js
+++ controllers/entry.js
@@ -4,57 +4,57 @@ const serviceAccount = require('../config/firebaseServiceAcc.json');
 const { GOOGLE_KEY } = process.env.GOOGLE_KEY || require('../config/index.js');
 
 
-
-const getDirections = async (event_id) => {
-  // Take data from database depending on id
-  // const app = firebase.app();
-  const db = admin.firestore();
-  const settings = { timestampsInSnapshots: true };
-  db.settings(settings);
-
-  const event = db.collection('tbl_trip').doc(event_id);
-  const getDoc = event.get()
-    .then(async (doc) => {
-      if (!doc.exists) {
-        console.log('No such document!');
-      } else {
-        const loc = doc.data().location.split(' ').join('');
-        const BASE_URL = `https://maps.googleapis.com/maps/api/directions/json?origin=Manchester,UK&destination=${loc},UK&mode=transit&key=${GOOGLE_KEY}`;
-        try {
-          const { data } = await axios.get(BASE_URL);
-          // console.log(data)
-          return data;
-        } catch (err) {
-          const data = { msg: `${err}` };
-          return data;
-        }
-      }
-    })
-    .catch((err) => {
-      console.log('Error getting document', err);
-    });
-};
-
-// This is triggered when the user clicks on an event
 exports.entryPoint = async (req, res) => {
 
-  const { event_id } = req.params;
-  const db = admin.firestore();
-  const newData = {
-    start_address: '',
-    departure_stop: '',
-    departure_time: '',
-    end_address: '',
-    arrival_stop: '',
-    arrival_time: '',
-    duration: '',
-    distance: '',
-    train_company: '',
+  const getDirections = async (event_id) => {
+    const db = admin.firestore();
+    const settings = { timestampsInSnapshots: true };
+    db.settings(settings);
+
+    const event = db.collection('tbl_trip').doc(event_id);
+    const getDoc = event.get()
+      .then(async (doc) => {
+        if (!doc.exists) {
+          console.log('No such document!');
+        } else {
+          const loc = doc.data().location.split(' ').join('');
+          const BASE_URL = `https://maps.googleapis.com/maps/api/directions/json?origin=Manchester,UK&destination=${loc},UK&mode=transit&key=${GOOGLE_KEY}`;
+          try {
+            const { data } = await axios.get(BASE_URL);
+
+            // const { event_id } = req.params;
+            // const db = admin.firestore();
+            // const newData = {
+            //   start_address: '',
+            //   departure_stop: '',
+            //   departure_time: '',
+            //   end_address: '',
+            //   arrival_stop: '',
+            //   arrival_time: '',
+            //   duration: '',
+            //   distance: '',
+            //   train_company: '',
+            // };
+
+            // const setDoc = db.collection('tbl_event').doc(event_id).set(newData);
+            res.send(data)
+          } catch (err) {
+            const data = { msg: `${err}` };
+            return data;
+          }
+        }
+      })
+      .catch((err) => {
+        console.log('Error getting document', err);
+      });
   };
-  const data1 = await getDirections(event_id)
-  console.log(data1)
-  return data1
 
 
-  // const setDoc = db.collection('tbl_event').doc(event_id).set(newData);
+
+  // const data1 = await getDirections(event_id)
+  // // console.log(data1)
+  // return data1
+
+
+  // 
 };
