6c8968c97f4023ee892d487a3295b13bcc2b65ac
Stefano Artoni <stefano@stefanos-mbp.home>
2018-12-01 15:10:53 +0000

    changed an s to see if its connetted with heroku

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -1,4 +1,4 @@
 exports.homepage = (req, res) => {
-  const homepageObj = { msg: 'CONNECTED', routes: ['/', '/events', '/event/:event_id', '/direction/:event_id'] };
+  const homepageObj = { msg: 'CONNECTED', routes: ['/', '/event', '/event/:event_id', '/direction/:event_id'] };
   res.send(homepageObj);
 };
