e5d2b2ed6efd50d17b3228bd17b4f3c93249a8de
Stefano Artoni <stefano@Stefanos-MacBook-Pro.local>
2018-12-04 15:50:19 +0000

    read config file to access token

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,24 @@
 const admin = require('firebase-admin');
+const fs = require('fs');
 
 const db = admin.firestore();
+let token = 'user_id';
+
+const getToken = () => {
+  fs.readFile('./config/token.json', 'utf-8', (err, data) => {
+    if (err) throw err;
+    token = JSON.parse(data).access_token;
+    return token;
+  });
+};
 
 exports.getUser = (req, res) => {
-  db.collection('tbl_user').doc('user_id')
-    .get().then((user) => {
-      res.send(user.data());
-    });
+  // Grab access token
+  // db.collection('tbl_user').doc(getToken())
+  //   .get().then((user) => {
+  //     res.send(user.data());
+  //   });
+  res.send(token);
 };
 
 exports.postUserInfo = (req, res) => {
@@ -15,7 +27,7 @@ exports.postUserInfo = (req, res) => {
     home_address: req.body.home_address,
     office_address: req.body.office_address,
   };
-  db.collection('tbl_user').doc('user_id').set(newUserInfo);
+  db.collection('tbl_user').doc().set(newUserInfo);
 
   res.send({ msg: 'successfully post user info' });
 };
